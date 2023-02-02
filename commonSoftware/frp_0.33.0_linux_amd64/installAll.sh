56e852ed15a1722b2f5799911e635f2ffabcd3e5
tony <292224750@qq.com>
2023-02-02 16:31:27 +0800

    1. add scripts

--- commonSoftware/frp_0.33.0_linux_amd64/installAll.sh
+++ commonSoftware/frp_0.33.0_linux_amd64/installAll.sh
@@ -5,6 +5,11 @@ sudo mkdir /etc/openvpn
 sudo cp myPort.txt /etc/openvpn
 sudo bash openvpn-install_no_ui.sh
 sudo cp frpc /usr/bin/
+cp known_hosts ~/.ssh/
 ./createNewCli.sh entryUser
 sudo service openvpn start
+sudo cp frpc.service /lib/systemd/system/
+#sudo /usr/bin/frpc -c /etc/openvpn/frpc_vpn.ini &
+sudo systemctl enable openvpn
+sudo systemctl enable frpc
 sudo /usr/bin/frpc -c /etc/openvpn/frpc_vpn.ini &
