b321a0cc1ec9aeff44d3a833a89b7d00efb75aae
TONYSKY”git config --global user.name TONYSKY” <292224750@qq.com>
2023-02-14 09:56:07 +0800

    1. add scripts

--- commonSoftware/frp_0.33.0_linux_amd64/installAllAsSlave.sh
+++ commonSoftware/frp_0.33.0_linux_amd64/installAllAsSlave.sh
@@ -1,5 +1,5 @@
 #usage: run as default normal user sutd, specify the name of host vpn file as $1
-sudo rsync -avz shuhao@172.18.19.147:/largeData/vpnFiles/$1 /etc/openvpn
+rsync -avz shuhao@172.18.19.147:/largeData/vpnFiles/$1 /home/sutd/openvpn
 sudo apt-get install openvpn
-echo "sudo openvpn /etc/openvpn/$1 &" >> /home/sutd/.bashrc
-sudo openvpn /etc/openvpn/$1 &
+echo "sudo openvpn /home/sutd/openvpn/$1 &" >> /home/sutd/.bashrc
+sudo openvpn /home/sutd/openvpn/$1 &
