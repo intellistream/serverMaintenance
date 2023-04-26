6b053d5ff19cc9b2dc43cdf12ab9313f1d8c8b78
tony <292224750@qq.com>
2023-04-26 19:20:03 +0800

    1. update server ip

---
+++ physicalMachineScripts/installCuda_2004.sh
@@ -0,0 +1,9 @@
+# for ubuntu 20.04 only
+export distro=ubuntu2004
+export arch=x86_64
+wget https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/cuda-keyring_1.0-1_all.deb
+sudo dpkg -i cuda-keyring_1.0-1_all.deb 
+sudo apt-get update
+sudo apt-get install cuda
+sudo apt-get install nvidia-gds
+sudo reboot
